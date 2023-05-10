7f8a8782868334116878003500dfa5863505607f
Replit user <>
2023-05-10 20:48:02 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -42,16 +42,20 @@ int is_equal(void* key1, void* key2){
 void insertMap(HashMap * map, char * key, void * value) {
 
   unsigned short indice = hash(key, map->capacity);
-  if (map->buckets[indice] == NULL && map->buckets[indice]->key == NULL){
+  if (map->buckets[indice] == NULL) {
+    map->buckets[indice] = malloc(sizeof(HashNode));
     map->buckets[indice]->key = key;
     map->buckets[indice]->value = value;
   }
-  else
-  {
-    while (map->buckets[indice] != NULL && map->buckets[indice]->key != NULL)
-    {
+  else if (map->buckets[indice]->key == NULL) {
+    map->buckets[indice]->key = key;
+    map->buckets[indice]->value = value;
+  }
+  else {
+    while (map->buckets[indice] != NULL && map->buckets[indice]->key != NULL) {
       indice = (indice + 1) % map->capacity;
     }
+    map->buckets[indice] = malloc(sizeof(HashNode));
     map->buckets[indice]->key = key;
     map->buckets[indice]->value = value;
   }
