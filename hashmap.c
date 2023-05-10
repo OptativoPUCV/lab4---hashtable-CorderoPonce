4b5c006757a0818a451b49bafbba4c1a45a44522
Replit user <>
2023-05-10 21:16:58 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -107,7 +107,9 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-
+  for (int i = 0 ; i < map->capacity ; i++){
+    if (map->buckets[i] != NULL) return map->buckets[i];
+  }
     return NULL;
 }
 
