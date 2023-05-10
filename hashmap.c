09056d02eb9af6638d2d51e2fea3c5d9b88570e0
Replit user <>
2023-05-10 21:20:28 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -108,9 +108,11 @@ Pair * searchMap(HashMap * map,  char * key) {
 
 Pair * firstMap(HashMap * map) {
   for (int i = 0 ; i < map->capacity ; i++){
-    if (map->buckets[i] != NULL) return map->buckets[i];
+    if (map->buckets[i] != NULL){
+      return map->buckets[i];
+    } else if(map->buckets[i]->key != NULL) return map->buckets[i];
   }
-    return NULL;
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
